05a99280f22370a120c5fa11e8cd448b30529486
Adella <adellakenn@gmail.com>
2019-05-16 15:32:54 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,6 +66,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday_name|
     puts "#{season.capitalze}:"
     holiday_name.each do |name, supply|
+      puts "#{name.to_s.split("_").map {|i| i.capitalize}.join(" ")}: #{item.join(", ")}"
 end
 
 def all_holidays_with_bbq(holiday_hash)
