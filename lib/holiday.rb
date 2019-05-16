2a58c0e247a2a3aeb75a0cee5918cca18706b27c
Adella <adellakenn@gmail.com>
2019-05-16 17:24:12 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,8 +61,7 @@ def all_supplies_in_holidays(holiday_hash)
       # binding.pry
     holiday_name.each do |name, supply|
       # binding.pry
-      puts "  #{name.to_s.split("_").map {|value| value.capitalize}.join(" ")}: #{supply.join(", ")}"   #starting with the first level hash, 
-                                                                                                        #then moving down, value == iteration
+      puts "  #{name.to_s.split("_").map {|value| value.capitalize}.join(" ")}: #{supply.join(", ")}"   #turn into string, then split 
                                                                                                         #underscore "_" is needed
                                                                                                         #to keep on one line
       
