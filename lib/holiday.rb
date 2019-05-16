1e12adff2432e149120b932eb3d1904f4dcbef78
Adella <adellakenn@gmail.com>
2019-05-16 17:02:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,7 +69,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-# array = [].flatten.compact
+
  holiday_hash.map do |season, holiday|
    holiday.map do |holiday, item|
     holiday if item.include?("BBQ")
