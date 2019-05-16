a90312dc71ca2cde7b5c2f3fb25b8a03087a242c
Adella <adellakenn@gmail.com>
2019-05-16 14:41:55 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -41,6 +41,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   # code here
   # remember to return the updated hash
   holiday_hash.each do |season_hash|
+    binding.pry
     if season == season_hash
       season << [holiday_name][supply_array]
     end
