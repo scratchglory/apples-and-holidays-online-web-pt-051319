986576310871a279bc01680b92df36fe3e9974a7
Adella <adellakenn@gmail.com>
2019-05-16 15:04:05 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season_name, holiday_name|
-    holiday_name.each do |fastival, supply|
+    holiday_name.each do |festival, supply|
       binding.pry
     end  
   end
